ef7c18fca9372485061f0a72e7fa7f3715a19c26
Paola Guarasci <paolaguarasci@gmail.com>
2018-09-17 09:48:40 +0200

    watermark ok, still position issue

--- app.js
+++ app.js
@@ -38,16 +38,15 @@ const hummus = require('hummus');
 
 
 // add watermark
-var pdfWriter = hummus.createWriterToModify(__dirname + '/TestMaterials/BasicJPGImagesTest.PDF', {
-    modifiedFilePath: __dirname + '/output/BasicJPGImagesTestPageModified.pdf'
+const logo = '/sample/nodejs.png';
+var pdfWriter = hummus.createWriterToModify('./sample/lorem1.pdf', {
+    modifiedFilePath: './output/watermarked.pdf'
 });
-
+var formXObject = pdfWriter.createFormXObjectFromPNG(__dirname + logo);
 var pageModifier = new hummus.PDFPageModifier(pdfWriter, 0);
-pageModifier.startContext().getContext().writeText(
-    'Test Text',
-    75, 805,
-    { font: pdfWriter.getFontForFile(__dirname + '/TestMaterials/fonts/Couri.ttf'), size: 14, colorspace: 'gray', color: 0x00 }
-);
-
+pageModifier.startContext().getContext().q()
+    // .cm(1, 0, 0, 1, 0, 400)
+    .doXObject(formXObject)
+    .Q();
 pageModifier.endContext().writePage();
 pdfWriter.end();
