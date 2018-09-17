651cf85986806307b46dd7551e6eadb65df643a2
Paola Guarasci <paolaguarasci@gmail.com>
2018-09-17 08:41:43 +0200

    merge works with array passing as args cms line

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const textract = require('textract');
 const pathFile = path.resolve(__dirname + '/sample/lorem1.pdf');
+const hummus = require('hummus');
 console.log(pathFile)
 fs.readFile(pathFile, (err, data) => {
     if (err) console.log(err.message)
@@ -11,3 +12,12 @@ fs.readFile(pathFile, (err, data) => {
         console.log(text);
     })
 });
+
+
+//merge
+const files = process.argv.slice(3);
+const pdfWriter = hummus.createWriter(__dirname + '/output/TestOnlyMerge.pdf');
+for (let i in files) {
+    pdfWriter.appendPDFPagesFromPDF(path.relative(__dirname, files[i]));
+}
+pdfWriter.end();
